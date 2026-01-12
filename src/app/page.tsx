ed94c8019d5c9fe0d2cd3d3d2cce3c5a066f6beb
Mark Thomas <tmarn2004@gmail.com>
2026-01-12 12:01:34 +0000

    Try fixing this error: `Runtime ReferenceError: isSessionActive is not d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
   };
 
   const toggleSession = () => {
-    if (isSessionActive) {
+    if (sessionActive) {
       setSessionActive(false);
       setSignedInStudents([]);
       setLecturerLocation(null);
